4afbc5a950f747a13e7b069de585417887cf19a7
Makesi Hamilton <whadup_hamilton@yahoo.com>
2016-07-29 16:27:52 +0000

    Initial commit of metadata files

---
+++ app.js
@@ -0,0 +1,68 @@
+// require and represent express
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+
+//Get config file for connection to mongod
+var config = require('./config')
+
+
+//Allows concatenation of paths
+var path = require('path');
+
+
+//Connect mongoose to mongodb 
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://'+config.db.host+'/'+config.db.name);
+
+
+//Allows for uploading of files in express
+var multer = require('multer');
+
+var upload = multer({dest: './upload'});
+
+
+
+// Deal with favicon request
+
+app.get('/favicon.ico', function(request, response){
+    
+    response.writeHead(200,{'Content-Type': 'image/x-icon'});
+    response.end;
+    return;
+    
+});
+
+
+
+//route to serve index.html
+
+app.get('/', function (request, response){
+    
+    response.sendFile(path.join(__dirname + '/index.html'));
+    console.log('File sent:', path.join(__dirname + '/index.html'));
+    
+});
+
+
+
+// use of multer dependancy
+app.post('/', upload.single('a_file'), function(request, response){
+    
+    console.log(request.file.size);
+    response.json({
+      'filename': request.file.originalname,
+      'size': request.file.size,
+    });
+	  response.status(204).end();
+    
+});
+
+
+
+
+var server = app.listen(port, function(){console.log('Listening on port:', port)});
---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {};
+
+config.db = {};
+
+//host name
+config.webhost = 'file-metadata-kesi2k.c9users.io'
+
+//MongoDB host and dbase name
+config.db.host = 'localhost';
+config.db.name = 'file-storage';
+
+
+module.exports = config;
